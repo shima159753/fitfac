d181b0ba4d9ee8a182d6b2d490f89126382dee39
shima159753 <shimazakib13@gmail.com>
2021-03-08 17:50:58 +0900

    メール修正1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fitfac_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:            ENV["SEND_MAIL"],
+  password:             ENV["SEND_MAIL_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
